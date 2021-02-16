b2ec7933d71d8ea187b34d7558bf65b6999f1351
Andre Hochi <andrehochiwork@gmail.com>
2021-02-16 04:27:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,12 +130,12 @@ def num_points_scored(input_player)
   score = 0
   game_hash[:home][:players].each do |player| 
     if input_player == player[:player_name]
-      score = player.points
+      score = player[:points]
     end
   end
   game_hash[:away][:players].each do |player| 
     if input_player == player[:player_name]
-      score = player.points
+      score = player[:points}
     end
   end
   score
